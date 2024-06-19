6818807383429823b1eb813a708532841a7c557c
TuanQuan-Dev <47420452+TuanQuan-Dev@users.noreply.github.com>
2024-06-19 15:23:12 +0700

    Add files via upload

---
+++ Cuong.py
@@ -0,0 +1,38 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+import warnings
+warnings.filterwarnings("ignore")
+
+#streamlit run Cuong.py
+
+# Dữ liệu 
+
+
+st.title("DỰ ĐOÁN kom")
+st.write("<br/>", unsafe_allow_html=True)
+
+st.header("Dữ liệu gốc:")
+
+X = np.array([0, 10, 20, 30, 40, 50, 60]) 
+y = np.array([32, 20, 12.5, 8, 4.8, 3, 2])
+ 
+plt.plot(X, y) 
+st.pyplot(plt)
+
+X = X.reshape(-1, 1)
+y = y.reshape(-1, 1)
+
+model = LinearRegression()
+model.fit(X, y)
+
+text = st.number_input("Vui lòng nhập nhiệt độ:")
+X_test = np.array([text])
+y = model.predict(X_test.reshape(-1, 1))[0][0]
+
+st.write(f"Kết quả: <b>{y}</b>", unsafe_allow_html=True)
